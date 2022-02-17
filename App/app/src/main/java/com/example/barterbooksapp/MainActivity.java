f5b8e3d89a49daaadf5df6ee88cb2c585aa0df7e
RoarshacH <vid.dilshan@gmail.com>
2022-02-17 14:01:39 -0500

    Card View

--- App/app/src/main/java/com/example/barterbooksapp/MainActivity.java
+++ App/app/src/main/java/com/example/barterbooksapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.barterbooksapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -12,7 +13,13 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private List<String> titles;
-    private List<String> images;
+    private List<Integer> images;
+    private List<String> authors;
+    private List<String> conditions;
+    private List<String> locations;
+    private List<String> prices;
+
+    private MyRecyclerViewAdapter adapter;
 
 
     @Override
@@ -23,10 +30,106 @@ public class MainActivity extends AppCompatActivity {
         //Remove action Bar
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
-            recyclerView = findViewById(R.id.recyclerView);
         }
+
+
+        recyclerView = findViewById(R.id.recyclerView);
 
         titles = new ArrayList<>();
         images = new ArrayList<>();
+        authors = new ArrayList<>();
+        conditions = new ArrayList<>();
+        locations = new ArrayList<>();
+        prices = new ArrayList<>();
+
+
+        images = initializeTestImages(images);
+        titles = initializeTestTitles(titles);
+
+        initializeOtherTitles();
+
+        adapter = new MyRecyclerViewAdapter(this, titles, images, authors, conditions, locations, prices);
+
+
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(gridLayoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setAdapter(adapter);
+
+
+
+    }
+
+    private List<Integer> initializeTestImages(List<Integer> imagesList){
+        //initialize images TEST only
+        imagesList.add((R.drawable.book1));
+        imagesList.add((R.drawable.book2));
+        imagesList.add((R.drawable.book3));
+        imagesList.add((R.drawable.book4));
+        imagesList.add((R.drawable.book5));
+        imagesList.add((R.drawable.book6));
+        imagesList.add((R.drawable.book7));
+        imagesList.add((R.drawable.book8));
+
+        return imagesList;
+    }
+
+    private List<String> initializeTestTitles(List<String> titlesList){
+        //initialize images TEST only
+        titlesList.add("Gardens Of The moon");
+        titlesList.add("Algebra and Geometry");
+        titlesList.add("Mathematics and The Real World");
+        titlesList.add("Fifth Season");
+        titlesList.add("Name of the Wind");
+        titlesList.add("What IF");
+        titlesList.add("Deep Learning With Python");
+        titlesList.add("Wise Man's Fear");
+
+        return titlesList;
+    }
+
+    private void initializeOtherTitles(){
+        //initialize others for testing TEST only
+        authors.add("Steven Erikson");
+        authors.add("Mark V. Lawson");
+        authors.add("Zvi Artstein");
+        authors.add("N. K Jemsin");
+        authors.add("Patrick Rothfuss");
+        authors.add("Randall Munroe");
+        authors.add("François Chollet");
+        authors.add("Patrick Rothfuss");
+
+
+        conditions.add("Used Like New");
+        conditions.add("Library");
+        conditions.add("Used");
+        conditions.add("New");
+        conditions.add("Used Like New");
+        conditions.add("Used Like New");
+        conditions.add("Used");
+        conditions.add("New");
+
+
+        locations.add("Barrie");
+        locations.add("Barrie");
+        locations.add("Barrie");
+        locations.add("Barrie");
+        locations.add("Barrie");
+        locations.add("Barrie");
+        locations.add("Barrie");
+        locations.add("Barrie");
+
+
+        prices.add("$8.99");
+        prices.add("$4.99");
+        prices.add("$6.00");
+        prices.add("$12.99");
+        prices.add("$10.00");
+        prices.add("$6.50");
+        prices.add("$9.00");
+        prices.add("$14.00");
+
     }
 }
---
+++ App/app/src/main/java/com/example/barterbooksapp/MyRecyclerViewAdapter.java
@@ -0,0 +1,82 @@
+package com.example.barterbooksapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.net.Inet4Address;
+import java.util.List;
+
+public class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.MyViewHolder> {
+    private Context context;
+    private List<String> titles;
+    private List<Integer> images;
+    private List<String> authors;
+    private List<String> conditions;
+    private List<String> locations;
+    private List<String> prices;
+
+    public MyRecyclerViewAdapter(Context context, List<String> titles, List<Integer> images, List<String> authors,
+                                 List<String> conditions, List<String> locations, List<String> prices){
+        this.context = context;
+        this.titles = titles;
+        this.images = images;
+        this.authors = authors;
+        this.conditions = conditions;
+        this.locations = locations;
+        this.prices = prices;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.grid_item,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.textView.setText(titles.get(position));
+        holder.imageView.setImageResource(images.get(position));
+        holder.authorTextView.setText(authors.get(position));
+        holder.conditionTextView.setText(conditions.get(position));
+        holder.locationTextView.setText(locations.get(position));
+        holder.priceTextView.setText(prices.get(position));
+        holder.barterCheckBox.setEnabled(Math.random() < 0.5);
+    }
+
+    @Override
+    public int getItemCount() {
+        return titles.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+        TextView textView;
+        TextView authorTextView;
+        TextView locationTextView;
+        TextView conditionTextView;
+        TextView priceTextView;
+        CheckBox barterCheckBox;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            textView = itemView.findViewById(R.id.titleText);
+            authorTextView = itemView.findViewById(R.id.authorText);
+            locationTextView = itemView.findViewById(R.id.locationText);
+            conditionTextView = itemView.findViewById(R.id.conditionText);
+            priceTextView = itemView.findViewById(R.id.priceText);
+            barterCheckBox = itemView.findViewById(R.id.barterCheckBox);
+        }
+    }
+}
