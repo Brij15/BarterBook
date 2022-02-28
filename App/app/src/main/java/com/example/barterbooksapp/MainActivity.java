3cbd3515d87d370bfdd373f27c55ddb9c324c0da
RoarshacH <smv.dilshan@gmail.com>
2022-02-28 14:37:00 -0500

    Using a data class to set the Posts

--- App/app/src/main/java/com/example/barterbooksapp/MainActivity.java
+++ App/app/src/main/java/com/example/barterbooksapp/MainActivity.java
@@ -17,13 +17,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
-    private List<String> titles;
-    private List<Integer> images;
-    private List<String> authors;
-    private List<String> conditions;
-    private List<String> locations;
-    private List<String> prices;
-
+    private List<BookPost> bookPosts;
     private MyRecyclerViewAdapter adapter;
     private BottomNavigationView bottomNavigationView;
 
@@ -39,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         bottomNavigationView = findViewById(R.id.bottomNavigationBar);
-        bottomNavigationView.setSelected(false);
+        bottomNavigationView.setSelectedItemId(R.id.go_home);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -47,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent;
                 switch (item.getItemId()){
                     case R.id.seller_list:
-                        intent = new Intent(MainActivity.this, SellerListActivity.class);
-                        startActivity(intent);
+//                        intent = new Intent(MainActivity.this, SellerListActivity.class);
+//                        startActivity(intent);
 //                        Not working Issue with Seller list Activity
                         break;
 
@@ -62,26 +56,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
+        bookPosts = new ArrayList<>();
+        initializeTestData();
         recyclerView = findViewById(R.id.recyclerView);
 
-        titles = new ArrayList<>();
-        images = new ArrayList<>();
-        authors = new ArrayList<>();
-        conditions = new ArrayList<>();
-        locations = new ArrayList<>();
-        prices = new ArrayList<>();
 
-
-        images = initializeTestImages(images);
-        titles = initializeTestTitles(titles);
-
-        initializeOtherTitles();
-
-        adapter = new MyRecyclerViewAdapter(this, titles, images, authors, conditions, locations, prices);
-
-
+        adapter = new MyRecyclerViewAdapter(this, bookPosts);
 
         GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(gridLayoutManager);
@@ -91,74 +71,16 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private List<Integer> initializeTestImages(List<Integer> imagesList){
+    private void initializeTestData(){
         //initialize images TEST only
-        imagesList.add((R.drawable.book1));
-        imagesList.add((R.drawable.book2));
-        imagesList.add((R.drawable.book3));
-        imagesList.add((R.drawable.book4));
-        imagesList.add((R.drawable.book5));
-        imagesList.add((R.drawable.book6));
-        imagesList.add((R.drawable.book7));
-        imagesList.add((R.drawable.book8));
-
-        return imagesList;
+        bookPosts.add(new BookPost("Gardens Of The moon",R.drawable.book1,"Steven Erikson", "Used Like New", "Barrie", 8.99 ));
+        bookPosts.add(new BookPost("Algebra and Geometry", R.drawable.book2,"Mark V. Lawson", "Library", "Barrie", 8.99));
+        bookPosts.add(new BookPost("Mathematics and The Real World",R.drawable.book3 ,"Zvi Artstein", "Used", "Barrie", 4.99));
+        bookPosts.add(new BookPost("Fifth Season",R.drawable.book4,"N. K Jemsin", "Used","Barrie", 6.00 ));
+        bookPosts.add(new BookPost("Name of the Wind", R.drawable.book5,"Patrick Rothfuss", "Used Like New", "Barrie", 12.99));
+        bookPosts.add(new BookPost("What IF", R.drawable.book6,"Randall Munroe", "Used Like New", "Barrie", 10.00));
+        bookPosts.add(new BookPost("Deep Learning With Python", R.drawable.book7 ,"François Chollet", "Used", "Barrie", 6.50));
+        bookPosts.add(new BookPost("Wise Man's Fear", R.drawable.book8, "Patrick Rothfuss", "New", "Barrie", 14.00));
     }
 
-    private List<String> initializeTestTitles(List<String> titlesList){
-        //initialize images TEST only
-        titlesList.add("Gardens Of The moon");
-        titlesList.add("Algebra and Geometry");
-        titlesList.add("Mathematics and The Real World");
-        titlesList.add("Fifth Season");
-        titlesList.add("Name of the Wind");
-        titlesList.add("What IF");
-        titlesList.add("Deep Learning With Python");
-        titlesList.add("Wise Man's Fear");
-
-        return titlesList;
-    }
-
-    private void initializeOtherTitles(){
-        //initialize others for testing TEST only
-        authors.add("Steven Erikson");
-        authors.add("Mark V. Lawson");
-        authors.add("Zvi Artstein");
-        authors.add("N. K Jemsin");
-        authors.add("Patrick Rothfuss");
-        authors.add("Randall Munroe");
-        authors.add("François Chollet");
-        authors.add("Patrick Rothfuss");
-
-
-        conditions.add("Used Like New");
-        conditions.add("Library");
-        conditions.add("Used");
-        conditions.add("New");
-        conditions.add("Used Like New");
-        conditions.add("Used Like New");
-        conditions.add("Used");
-        conditions.add("New");
-
-
-        locations.add("Barrie");
-        locations.add("Barrie");
-        locations.add("Barrie");
-        locations.add("Barrie");
-        locations.add("Barrie");
-        locations.add("Barrie");
-        locations.add("Barrie");
-        locations.add("Barrie");
-
-
-        prices.add("$8.99");
-        prices.add("$4.99");
-        prices.add("$6.00");
-        prices.add("$12.99");
-        prices.add("$10.00");
-        prices.add("$6.50");
-        prices.add("$9.00");
-        prices.add("$14.00");
-
-    }
 }
