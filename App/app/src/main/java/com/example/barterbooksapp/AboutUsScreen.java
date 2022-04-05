a5ececf9f3063b1454b29270fd68475f2b6d88ad
sanchitsingh <sanchit1498@gmail.com>
2022-04-05 14:36:32 -0400

    Registration screen update

---
+++ App/app/src/main/java/com/example/barterbooksapp/AboutUsScreen.java
@@ -0,0 +1,26 @@
+package com.example.barterbooksapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class AboutUsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us_screen);
+    }
+
+    public void email(View view){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("plain/text");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "abc@gmail.com" });
+        intent.putExtra(Intent.EXTRA_SUBJECT, "subject");
+        intent.putExtra(Intent.EXTRA_TEXT, "mail body");
+        startActivity(Intent.createChooser(intent, ""));
+
+    }
+}
