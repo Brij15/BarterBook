a8f0e228e0e377bbadb6cc8c81b755f5d2d5798c
RoarshacH <smv.dilshan@gmail.com>
2022-06-06 08:07:47 -0400

    Search Function done

--- App/app/src/main/java/com/example/barterbooksapp/MainActivity.java
+++ App/app/src/main/java/com/example/barterbooksapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.barterbooksapp;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -15,7 +14,6 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -23,26 +21,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.barterbooksapp.recyclerViewAdapters.MyRecyclerViewAdapter;
 import com.example.barterbooksapp.utlity.BookPostDataModel;
-import com.example.barterbooksapp.utlity.DBUtilities;
 import com.example.barterbooksapp.utlity.FilterUtilities;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
