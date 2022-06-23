22a4083c8b18045caee35b5d52fa3fb7b4de048f
RoarshacH <smv.dilshan@gmail.com>
2022-06-23 15:02:37 -0400

    DeleteAD

--- App/app/src/main/java/com/example/barterbooksapp/DetailsActivity.java
+++ App/app/src/main/java/com/example/barterbooksapp/DetailsActivity.java
@@ -1,21 +1,29 @@
 package com.example.barterbooksapp;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.barterbooksapp.utlity.BookPostDataModel;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,6 +31,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -43,8 +52,10 @@ public class DetailsActivity extends AppCompatActivity {
     ImageSlider imageSlider;
     String userEmail;
     BookPostDataModel book;
+    String filterValue;
 
     private BottomNavigationView bottomNavigationView;
+    FirebaseFirestore db;
 
 
     @SuppressLint("NonConstantResourceId")
@@ -52,7 +63,7 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         titleText = findViewById(R.id.bookdetailTitle);
         authorText = findViewById(R.id.authorDetailText);
@@ -71,7 +82,7 @@ public class DetailsActivity extends AppCompatActivity {
 
         if (thisIntent.hasExtra("postID")){
             if (!thisIntent.getStringExtra("postID").isEmpty()){
-                String filterValue = thisIntent.getStringExtra("postID");
+                filterValue = thisIntent.getStringExtra("postID");
                 DocumentReference docRef = db.collection("BarterBooksDB").document(filterValue);
                 docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
@@ -137,9 +148,23 @@ public class DetailsActivity extends AppCompatActivity {
             }
             return false;
         });
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+
 
         ImageButton emailBtn = findViewById(R.id.emailUser);
         ImageButton notifyBtn = findViewById(R.id.notifyUser);
+
+        ImageButton editBtn = findViewById(R.id.editAd);
+        ImageButton deleteBtn = findViewById(R.id.deleteAd);
+
+        if (user.getUid() == book.getUserID()){
+            emailBtn.setVisibility(View.GONE);
+            notifyBtn.setVisibility(View.GONE);
+            editBtn.setVisibility(View.VISIBLE);
+            deleteBtn.setVisibility(View.VISIBLE);
+        }
+
         emailBtn.setOnClickListener(v -> {
             Uri uri = Uri.parse("mailto:" + userEmail)
                     .buildUpon()
@@ -153,7 +178,7 @@ public class DetailsActivity extends AppCompatActivity {
         notifyBtn.setOnClickListener(v -> {
             // The topic name can be optionally prefixed with "/topics/".
             String topic = book.getUserID();
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
             if (user != null) {
                 // Check if user's email is verified
 //            boolean emailVerified = user.isEmailVerified();
@@ -176,10 +201,67 @@ public class DetailsActivity extends AppCompatActivity {
 //                System.out.println("Successfully sent message: " + response);
                 Toast.makeText(this,"Successfully sent message", Toast.LENGTH_SHORT).show();
             }
-
-
-
         });
+
+        editBtn.setOnClickListener(v -> {
+            Uri uri = Uri.parse("mailto:" + userEmail)
+                    .buildUpon()
+                    .appendQueryParameter("subject", titleText.getText() + "Book Ad on Barter Books")
+                    .build();
+
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, uri);
+            startActivity(Intent.createChooser(emailIntent, "Select your Email app"));
+        });
+
+        deleteBtn.setOnClickListener(v -> {
+            deleteAlert();
+        });
+
+
+    }
+
+    private void deleteAlert(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//            builder.setCancelable(true);
+        builder.setMessage("Do you really want to delete this Ad?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        if(filterValue != null){
+                            db.collection("BarterBooksDB").document(filterValue)
+                                    .delete()
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            finish();
+                                            Intent intent = new Intent(DetailsActivity.this, MainActivity.class);
+                                            startActivity(intent);
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(getApplicationContext(),"Error Deleting AD",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                            Toast.makeText(getApplicationContext(),"Ad Deleted",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //  Action for 'NO' Button
+                        dialog.cancel();
+                    }
+                });
+        //Creating dialog box
+        AlertDialog alert = builder.create();
+        //Setting the title manually
+        alert.setTitle("Delete Ad");
+        alert.show();
     }
 
 
