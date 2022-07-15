e198cf7a7b637467f8a8ee5dd3bf17371c065095
Aditi Saini <sainiaditi67@gmail.com>
2022-07-14 22:12:00 -0700

    Push notification completed

--- App/app/src/main/java/com/example/barterbooksapp/FirebasenotificationsService.java
+++ App/app/src/main/java/com/example/barterbooksapp/FirebasenotificationsService.java
@@ -1,15 +1,45 @@
 package com.example.barterbooksapp;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.os.Build;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class FirebasenotificationsService extends FirebaseMessagingService {
+    private static final String CHANNEL_ID ="notifyme";
+    private int notiId = 0;
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // TODO(developer): Handle FCM messages here
         Log.d("Firebase", "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        createLocalNotificationChannel();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody())
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        notificationManager.notify(notiId++, builder.build());
+    }
+    private void createLocalNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 }
+
+
